408426bad2a97b40be3632eaf733b932bd4e8706
Carlos Eduardo <166134345+CarlosssEduardo@users.noreply.github.com>
2024-09-09 12:58:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,62 @@
+let dados = [
+  {
+    titulo: "Charles Oliveira",
+    descricao: "Charles do Bronx, guiado pela fé inabalável em Deus, é um exemplo de resiliência e determinação. A frase que ecoou mundialmente, Pode bater no cara, aqui aguenta, encapsula a força de vontade do lutador para superar qualquer adversidade. Sua jornada inspiradora, marcada por momentos de glória e superação, o tornou um ícone do MMA mundial.",
+    link:"https://pt.m.wikipedia.org/wiki/Charles_Oliveira target=_blank", 
+    video:"https://youtube.com/shorts/2JUxIDqLnwc?si=1Sj5wTFLKNfn3IPy"
+  },
+  {
+    titulo: "Islam Makhachev",
+    descricao:"Conhecido por sua luta de chão impecável, Islam Makhachev é um dos principais nomes do peso-leve no MMA. Discípulo de Khabib Nurmagomedov, ele carrega a tradição da luta russa e tem se destacado por suas performances dominantes no octógono.",
+    link: "https://pt.m.wikipedia.org/wiki/Islam_Makhachev target=_blank",
+    video:"https://youtube.com/shorts/e_zLd2_Qdpc?si=nBcVNZLqgIxScWSg"
+  },
+  {
+    titulo: "Aljamain Sterling",
+    descricao: " Aljamain Sterling, o Funk Master, é o atual campeão peso galo do UFC. Conhecido por sua luta de chão impecável e resistência, Sterling conquistou o título de forma controversa e tem se consolidado como um dos principais nomes da divisão.",
+    link: "https://pt.m.wikipedia.org/wiki/Aljamain_Sterling target=_blank",
+    video:"https://youtube.com/shorts/j1m1zkP4Ss8?si=NoDfW2BtLtFUXdg3"
+  },
+  {
+    titulo: "Sean O'Malley",
+    descricao: "Sean O'Malley é um dos prospectos mais promissores do peso galo. Com um estilo de luta agressivo e nocautes espetaculares, Sugar tem conquistado uma legião de fãs e se aproxima rapidamente de uma disputa de título.",
+    link: "https://pt.m.wikipedia.org/wiki/Sean_O%27Malley target=_blank",
+    video:"https://youtube.com/shorts/L5Qi5x0jKi0?si=UOsjZGkAeOIPqPjJ"
+  },
+  {
+    titulo: "Beneil Dariush",
+    descricao: "Beneil Dariush é um dos lutadores mais completos do peso leve. Com um jogo de chão versátil e um striking preciso, Dariush tem se destacado pela consistência e está sempre na disputa por uma chance ao título.",
+    link: "https://pt.m.wikipedia.org/wiki/Beneil_Dariush target=_blank",
+    video:"https://youtube.com/shorts/zH5vHpnwU-8?si=1Zf0R4OcqGfe_Wqn"
+  },
+  {
+    titulo: "Belal Muhammad",
+    descricao: "Belal Muhammad é conhecido por seu wrestling dominante e alta taxa de decisão. O Remember the Name tem escalado o ranking do peso-médio de forma consistente e é considerado um dos principais desafiantes ao título.",
+    link: "https://pt.m.wikipedia.org/wiki/Belal_Muhammad target=_blank",
+    video:"https://youtube.com/shorts/rqT6OL7dsEY?si=py0VNZtQUafC_MBK"
+  },
+  {
+    titulo: "Mateusz Gamrot",
+    descricao: "Mateusz Gamrot é um dos prospectos mais promissores do peso leve. O polonês possui um jogo de chão versátil e um striking em constante evolução, o que o torna um adversário desafiador para qualquer oponente.",
+    link: "https://pt.m.wikipedia.org/wiki/Mateusz_Gamrot target=_blank",
+    video:"https://youtube.com/shorts/ciXYT8DO7yU?si=2sQso2yjV6sUo-Rc"
+  },
+  {
+    titulo: "Sean Brady",
+    descricao: "Sean Brady é um dos lutadores mais promissores do peso-médio. Conhecido por sua luta de chão dominante e sua capacidade de finalizar lutas, Brady tem se destacado pela consistência e está subindo rapidamente no ranking.",
+    link: "https://pt.m.wikipedia.org/wiki/Sean_Brady target=_blank",
+    video:"https://youtube.com/shorts/NvmIkwsyaSo?si=zPpofjS3IigDFg7q"
+  },
+  {
+    titulo: "Jon Jones",
+    descricao: "Petr Yan, o No Mercy, é um ex-campeão peso galo do UFC. Conhecido por seu striking preciso e nocautes espetaculares, Yan é considerado um dos melhores strikers da divisão.",
+    link: "https://pt.m.wikipedia.org/wiki/Jon_Jones target=_blank",
+    video:"https://youtube.com/shorts/iIBb_VoPOzY?si=1YTXa2gNssYWht4Y"
+  },
+  {
+    titulo: "TJ Dillashaw",
+    descricao: "TJ Dillashaw é um ex-campeão peso galo do UFC. Conhecido por sua velocidade e precisão nos golpes, Dillashaw é um dos lutadores mais experientes e condecorados da divisão.",
+    link: "https://pt.m.wikipedia.org/wiki/TJ_Dillashaw target=_blank",
+    video:"https://youtube.com/shorts/usKXw_mQ7fk?si=LxCy0X6BtWa3u1Ki"
+  },
+  ];
