5906f116eb773d3c083c972dda54cbe1f65525a9
Carlos Eduardo <166134345+CarlosssEduardo@users.noreply.github.com>
2024-09-13 18:01:04 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,60 +3,60 @@ let dados = [
     titulo: "Charles Oliveira",
     descricao: "Charles do Bronx, guiado pela fé inabalável em Deus, é um exemplo de resiliência e determinação. A frase que ecoou mundialmente, Pode bater no cara, aqui aguenta, encapsula a força de vontade do lutador para superar qualquer adversidade. Sua jornada inspiradora, marcada por momentos de glória e superação, o tornou um ícone do MMA mundial.",
     link:"https://pt.m.wikipedia.org/wiki/Charles_Oliveira target=_blank", 
-    video:"https://youtube.com/shorts/2JUxIDqLnwc?si=1Sj5wTFLKNfn3IPy"
+    video:"https://www.tiktok.com/@.mmadome/video/7317296559807368455?is_from_webapp=1&sender_device=pc&web_id=7414201456449013254 target=_blank"
   },
   {
     titulo: "Islam Makhachev",
     descricao:"Conhecido por sua luta de chão impecável, Islam Makhachev é um dos principais nomes do peso-leve no MMA. Discípulo de Khabib Nurmagomedov, ele carrega a tradição da luta russa e tem se destacado por suas performances dominantes no octógono.",
     link: "https://pt.m.wikipedia.org/wiki/Islam_Makhachev target=_blank",
-    video:"https://youtube.com/shorts/e_zLd2_Qdpc?si=nBcVNZLqgIxScWSg"
+    video:"https://www.tiktok.com/@7foka_mma/video/7334052965260561696?is_from_webapp=1&sender_device=pc&web_id=7414201456449013254 target=_blank"
   },
   {
     titulo: "Aljamain Sterling",
     descricao: " Aljamain Sterling, o Funk Master, é o atual campeão peso galo do UFC. Conhecido por sua luta de chão impecável e resistência, Sterling conquistou o título de forma controversa e tem se consolidado como um dos principais nomes da divisão.",
     link: "https://pt.m.wikipedia.org/wiki/Aljamain_Sterling target=_blank",
-    video:"https://youtube.com/shorts/j1m1zkP4Ss8?si=NoDfW2BtLtFUXdg3"
+    video:"https://www.tiktok.com/@shiny.mma/video/7225646047861722394?is_from_webapp=1&sender_device=pc&web_id=7414201456449013254 target=_blank"
   },
   {
     titulo: "Sean O'Malley",
     descricao: "Sean O'Malley é um dos prospectos mais promissores do peso galo. Com um estilo de luta agressivo e nocautes espetaculares, Sugar tem conquistado uma legião de fãs e se aproxima rapidamente de uma disputa de título.",
     link: "https://pt.m.wikipedia.org/wiki/Sean_O%27Malley target=_blank",
-    video:"https://youtube.com/shorts/L5Qi5x0jKi0?si=UOsjZGkAeOIPqPjJ"
+    video:"hhttps://www.tiktok.com/@kingbury16/video/7269717620310412545?is_from_webapp=1&sender_device=pc&web_id=7414201456449013254ttps://youtube.com/shorts/L5Qi5x0jKi0?si=UOsjZGkAeOIPqPjJ target=_blank"
   },
   {
     titulo: "Beneil Dariush",
     descricao: "Beneil Dariush é um dos lutadores mais completos do peso leve. Com um jogo de chão versátil e um striking preciso, Dariush tem se destacado pela consistência e está sempre na disputa por uma chance ao título.",
     link: "https://pt.m.wikipedia.org/wiki/Beneil_Dariush target=_blank",
-    video:"https://youtube.com/shorts/zH5vHpnwU-8?si=1Zf0R4OcqGfe_Wqn"
+    video:"hthttps://www.tiktok.com/@warrrios.ufc/video/7305836252576648454?is_from_webapp=1&sender_device=pc&web_id=7414201456449013254tps://youtube.com/shorts/zH5vHpnwU-8?si=1Zf0R4OcqGfe_Wqn target=_blank"
   },
   {
     titulo: "Belal Muhammad",
     descricao: "Belal Muhammad é conhecido por seu wrestling dominante e alta taxa de decisão. O Remember the Name tem escalado o ranking do peso-médio de forma consistente e é considerado um dos principais desafiantes ao título.",
     link: "https://pt.m.wikipedia.org/wiki/Belal_Muhammad target=_blank",
-    video:"https://youtube.com/shorts/rqT6OL7dsEY?si=py0VNZtQUafC_MBK"
+    video:"https://https://www.tiktok.com/@fighting.focus/video/7256925051604274459?is_from_webapp=1&sender_device=pc&web_id=7414201456449013254youtube.com/shorts/rqT6OL7dsEY?si=py0VNZtQUafC_MBK target=_blank"
   },
   {
     titulo: "Mateusz Gamrot",
     descricao: "Mateusz Gamrot é um dos prospectos mais promissores do peso leve. O polonês possui um jogo de chão versátil e um striking em constante evolução, o que o torna um adversário desafiador para qualquer oponente.",
     link: "https://pt.m.wikipedia.org/wiki/Mateusz_Gamrot target=_blank",
-    video:"https://youtube.com/shorts/ciXYT8DO7yU?si=2sQso2yjV6sUo-Rc"
+    video:"https://youtube.com/shorts/ciXYT8DO7yU?si=2sQso2yjV6sUo-Rc target=_blank"
   },
   {
     titulo: "Sean Brady",
     descricao: "Sean Brady é um dos lutadores mais promissores do peso-médio. Conhecido por sua luta de chão dominante e sua capacidade de finalizar lutas, Brady tem se destacado pela consistência e está subindo rapidamente no ranking.",
     link: "https://pt.m.wikipedia.org/wiki/Sean_Brady target=_blank",
-    video:"https://youtube.com/shorts/NvmIkwsyaSo?si=zPpofjS3IigDFg7q"
+    video:"https://youtube.com/shorts/NvmIkwsyaSo?si=zPpofjS3IigDFg7q target=_blank"
   },
   {
     titulo: "Jon Jones",
     descricao: "Petr Yan, o No Mercy, é um ex-campeão peso galo do UFC. Conhecido por seu striking preciso e nocautes espetaculares, Yan é considerado um dos melhores strikers da divisão.",
     link: "https://pt.m.wikipedia.org/wiki/Jon_Jones target=_blank",
-    video:"https://youtube.com/shorts/iIBb_VoPOzY?si=1YTXa2gNssYWht4Y"
+    video:"https://youtube.com/shorts/iIBb_VoPOzY?si=1YTXa2gNssYWht4Y target=_blank"
   },
   {
     titulo: "TJ Dillashaw",
     descricao: "TJ Dillashaw é um ex-campeão peso galo do UFC. Conhecido por sua velocidade e precisão nos golpes, Dillashaw é um dos lutadores mais experientes e condecorados da divisão.",
     link: "https://pt.m.wikipedia.org/wiki/TJ_Dillashaw target=_blank",
-    video:"https://youtube.com/shorts/usKXw_mQ7fk?si=LxCy0X6BtWa3u1Ki"
+    video:"https://youtube.com/shorts/usKXw_mQ7fk?si=LxCy0X6BtWa3u1Ki target=_blank"
   },
   ];
